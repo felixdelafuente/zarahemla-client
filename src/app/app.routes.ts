eb669e019b8c6734d6c6cff7f0475164bb9af5e9
Felix Dela Fuente <felixjerome.delafuente@gmail.com>
2024-11-12 20:42:11 +0800

    init: environment setup

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    // component: 
+    pathMatch: 'full'
+  },
+  {
+    path: 'client',
+    // component:
+    children: [
+      {
+        path: 'dashboard',
+        // component:
+      }
+    ]
+  }
+];
