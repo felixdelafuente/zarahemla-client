51c7b23352f2de41cc76da65680a42a09255fc1d
Felix Dela Fuente <felixjerome.delafuente@gmail.com>
2024-12-26 17:25:18 +0800

    fix: environment url

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: false,
-  baseApiUrl: 'http://localhost:5158/api',
+  baseApiUrl: 'https://zarahemla-api.onrender.com/api',
+  // baseApiUrl: 'http://localhost:5158/api',
   tokenKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkZlbGl4IERlbGEgRnVlbnRlIiwiaWF0Ijo5ODM0Mjc0fQ.CR2QuMQZR8Hio4kZp5bWRKEhxznAxrQJO71yFu8V-lM'
 }
